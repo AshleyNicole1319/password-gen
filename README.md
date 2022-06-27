47bcb6ab6e35ce59f604586b55398af51a0e4143
AshleyNicole1319 <a-warford96@comcast.net>
2022-06-27 11:04:12 -0400

    completed project (worked offline for build)

---
+++ README.md
@@ -0,0 +1,20 @@
+# Password Generator
+
+## Purpose
+Create a password generating web app that allows users to select the criteria for their password (cases, numbers, special characters). Utilizes JavaScript for showing prompts upon button click.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website URL
+https://ashleynicole1319.github.io/password-gen/
+
+## Screen Capture of Password Generator Webpage
+<img src="./assets/screenshot.png">
+
+## Contribution
+Edited by Ashley Warford
+
+#### Module 3- Challenge 3
---
+++ assets/script.js
@@ -0,0 +1,66 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+    // list arrays of all characters & symbols available to generate password
+    var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var symbol = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
+    var userChoice = [];
+
+    // desired password length
+    var characterCount = window.prompt("How many characters would you like your password to be? Please choose a length between 8 and 128.");
+
+    // ensure password length is 8-128 characters
+    if (isNaN(characterCount) || characterCount < 8 || characterCount > 128) {
+        window.alert("Password length must be a number between 8 - 128. Please try again.");
+        return generatePassword();
+    }
+
+    // list of user input variables
+    var confirmLowercase = window.confirm("Would you like to include lowercase letters in your password?");
+    var confirmUppercase = window.confirm("Would you like to include uppercase letters in your password?");
+    var confirmNumbers = window.confirm("Would you like to include numbers in your password?");
+    var confirmSymbols = window.confirm("Would you like to include special characters in your password?");
+
+    // include lowercase, uppercase, and/or special characters
+    if (confirmLowercase) {
+        userChoice = userChoice.concat(alphaLower);
+    }
+    if (confirmUppercase) {
+        userChoice = userChoice.concat(alphaUpper);
+    }
+    if (confirmNumbers) {
+        userChoice = userChoice.concat(number);
+    }
+    if (confirmSymbols) {
+        userChoice = userChoice.concat(symbol);
+    }
+
+    // validate input
+    console.log(userChoice);
+
+    // generate password
+    var newPassword = "";
+    for (var i = 0; i < characterCount; i++) {
+        var randomChar = Math.floor(Math.random() * userChoice.length);
+        newPassword = newPassword + userChoice[randomChar];
+    }
+
+    // display generated password
+    return newPassword;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
